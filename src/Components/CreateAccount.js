53f0596002487211e77ec4bdc63f721da6ec401f
commando01000 <jfijcc124@gmail.com>
2023-07-29 10:30:15 +0300

    First-Commit

---
+++ src/Components/CreateAccount.js
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+function CreateAccount() {
+  return (
+    <MDBContainer fluid>
+      <MDBRow className="d-flex justify-content-center align-items-center h-100">
+        <MDBCol col="12">
+          <MDBCard
+            className="bg-dark text-white my-5 mx-auto"
+            style={{ borderRadius: "1rem", maxWidth: "400px" }}
+          >
+            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+              <h2 className="fw-bold mb-2 text-uppercase">Create An Account</h2>
+              <p className="text-white-50 mb-5">
+                Please Fill in the Form Below !
+              </p>
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="First Name"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="Last Name"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="Email address"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="Password"
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
+
+              <MDBBtn outline className="mx-2 px-5" color="white" size="lg">
+                Create Account
+              </MDBBtn>
+              <div className="d-flex flex-row mt-3 mb-5">
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="facebook-f" size="lg" />
+                </MDBBtn>
+
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="twitter" size="lg" />
+                </MDBBtn>
+
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "white" }}
+                >
+                  <MDBIcon fab icon="google" size="lg" />
+                </MDBBtn>
+              </div>
+
+              <div>
+                <p className="mb-0">
+                  Already Have an Account ? <Link to="/Login">Sign in</Link>
+                </p>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default CreateAccount;
